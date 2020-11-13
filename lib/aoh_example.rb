627badefb4b2a503d86f6c7ce0ca6549a4f15f06
plony benav <menachemfuterfas@gmail.com>
2020-11-13 19:00:57 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,6 +21,7 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  array_of_h = []
 end
 
 def aoh_lookup(aoh, row, key)
